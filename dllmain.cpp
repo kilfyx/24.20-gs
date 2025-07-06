520697c26d9ec70d478d24cbf487850fc5661566
kilfyx <kilfyx8@gmail.com>
2025-07-06 23:03:58 +0100

    fire

---
+++ dllmain.cpp
@@ -0,0 +1,22 @@
+#include "options.h"
+
+if (main::gun.bCreative)
+{
+
+  1:1 creative
+  
+}
+
+if (main::gun.b24)
+{
+
+  1:1 Season 24
+  
+}
+
+if (main::gun.bLategame)
+{
+
+  1:1 Lategame
+  
+}
